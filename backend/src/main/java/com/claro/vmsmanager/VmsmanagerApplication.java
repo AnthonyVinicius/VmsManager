cceb7e472fddf7deaa689478962f88c105b873e2
Anthony <anthonyvinicius1312@gmail.com>
2026-01-20 17:03:25 -0300

    chore: initial commit with Angular 17 frontend and Spring Boot backend

---
+++ backend/src/main/java/com/claro/vmsmanager/VmsmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.claro.vmsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmsmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmsmanagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/claro/vmsmanager/VmsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claro.vmsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
